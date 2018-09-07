1f8ca291ee43711583adba3802eee944cf8cc17f
yanghoon <ssang276@hanmail.net>
2018-09-07 16:15:14 +0900

    Show each resoruce data in table (dynamic)

--- src/components/Resources.vue
+++ src/components/Resources.vue
@@ -3,18 +3,7 @@
     <v-layout row wrap align-center justify-center>
       <v-flex xs11>
 
-        <!--
-        <v-breadcrumbs divider="-">
-          <v-breadcrumbs-item v-for="(sel, index) in select" :key="index">
-            <v-select :items="sel.items"
-                item-text="name" item-value="name"
-                :label="sel.label" v-model="sel.selected" @change="changeCluster">
-            </v-select>
-
-          </v-breadcrumbs-item>
-        </v-breadcrumbs>
-        -->
-
+      <!-- Cluster & Resource Navigation -->
         <v-layout row wrap align-center justify-start>
           <v-flex xs2>
             <v-select :items="select[0].items"
@@ -40,57 +29,78 @@
           </v-flex>
         </v-layout>
 
+        <!-- Main Data Table -->
         <v-card>
           <v-card-title>
-            Pods
+            <!-- Kind: Pods -->
             <v-spacer></v-spacer>
             <v-text-field v-model="table.keyword" @keyup.esc="table.keyword = undefined"
                           hide-details clearable
                           append-icon="search" label="Search"></v-text-field>
           </v-card-title>
 
-          <v-data-table :headers="headers" :items="table.items" :loading="table.loading" :search="table.keyword" hide-actions class="elevation-1">
+          <v-data-table :headers="table.headers" :items="table.items" :loading="table.loading" :search="table.keyword" hide-actions class="elevation-1">
             <v-progress-linear slot="progress" color="blue" indeterminate></v-progress-linear>
 
             <template slot="items" slot-scope="props">
-              <td class="text-xs-left">
-                <!-- https://router.vuejs.org/kr/guide/essentials/navigation.html -->
-                <router-link :to="`/${select[2].selected}/${props.item.metadata.name}?cs=${select[0].selected}&ns=${select[1].selected}`">
-                  {{ props.item.metadata.name }}
-                </router-link>
-              </td>
-              <td class="text-xs-center">{{ "-" }}</td>
-              <td class="text-xs-center">{{ props.item.status ? props.item.status.phase : '-' }}</td>
-              <td class="text-xs-center">{{ "-" }}</td>
-              <!-- <td class="text-xs-center">{{ props.item.status.startTime }}</td> -->
-              <td class="text-xs-center">{{ props.item.metadata.creationTimestamp | moment('from', true) }}</td>
-              <td class="text-xs-center">
+              <td v-for="meta in table.headers" :class="`text-xs-${meta.align}`">
+                <!-- COMMON: human readable time -->
+                <template v-if="meta.slot == 'from-now'">
+                  {{ _.property(meta.value.split('.'))(props.item) | moment('from', true) || '-' }}
+                </template>
 
-                <v-tooltip right :open-delay="0" :close-delay="0">
-                  <v-btn slot="activator" color="primary" flat icon
-                         @click="showEditor(props.item)">
-                    <v-icon>create</v-icon>
-                  </v-btn>
-                  <span>Edit Yaml</span>
-                </v-tooltip>
+                <!-- COMMON: resource detail view link -->
+                <template v-else-if="meta.text == 'Name'">
+                  <router-link :to="`/${select[2].selected}/${props.item.metadata.name}?cs=${select[0].selected}&ns=${select[1].selected}`">
+                    {{ _.property(meta.value.split('.'))(props.item) }}
+                  </router-link>
+                </template>
 
-                <v-tooltip right :open-delay="0" :close-delay="0"
-                           v-if="select[2].selected == 'pod'">
-                  <v-btn slot="activator" color="primary" flat icon
-                         @click="showSsh(props.item)">
-                    <v-icon>web_asset</v-icon>
-                  </v-btn>
-                  <span>Open Shell</span>
-                </v-tooltip>
+                <!-- COMMON: resource detail view link -->
+                <template v-else-if="meta.slot == 'label'">
+                  <div v-for="(v, k) in _.property(meta.value.split('.'))(props.item)" :key="k">
+                    <strong>{{ k }}:</strong>
+                    <span>{{ v }}</span>
+                  </div>
+                </template>
 
-                <v-tooltip right :open-delay="0" :close-delay="0"
-                           v-if="select[2].selected == 'pod'">
-                  <v-btn slot="activator" color="primary" flat icon
-                         @click="showLog(props.item)">
-                    <v-icon>notes</v-icon>
-                  </v-btn>
-                  <span>Logs</span>
-                </v-tooltip>
+                <!-- COMMON: tools -->
+                <template v-else-if="meta.slot == 'action'">
+                  <v-tooltip right :open-delay="0" :close-delay="0">
+                    <v-btn slot="activator" color="primary" flat icon @click="showEditor(props.item)">
+                      <v-icon>create</v-icon>
+                    </v-btn>
+                    <span>Edit Yaml</span>
+                  </v-tooltip>
+
+                  <v-tooltip right :open-delay="0" :close-delay="0" v-if="select[2].selected == 'pod'">
+                    <v-btn slot="activator" color="primary" flat icon @click="showSsh(props.item)">
+                      <v-icon>web_asset</v-icon>
+                    </v-btn>
+                    <span>Open Shell</span>
+                  </v-tooltip>
+
+                  <v-tooltip right :open-delay="0" :close-delay="0" v-if="select[2].selected == 'pod'">
+                    <v-btn slot="activator" color="primary" flat icon @click="showLog(props.item)">
+                      <v-icon>notes</v-icon>
+                    </v-btn>
+                    <span>Logs</span>
+                  </v-tooltip>
+                </template>
+
+                <!-- Deploy -->
+                <template v-else-if="meta.slot == 'replica'">
+                  {{ props.item.status.replicas }} / {{ props.item.status.availableReplicas }}
+                </template>
+                <template v-else-if="meta.slot == 'images'">
+                  <div v-for="img in _.map(props.item.spec.template.spec.containers, _.property('image'))">
+                    {{ img }}
+                  </div>
+                </template>
+
+                <template v-else>
+                  {{ _.property(meta.value.split('.'))(props.item) || '-' }}
+                </template>
               </td>
             </template>
 
@@ -103,6 +113,8 @@
       </v-flex>
     </v-layout>
 
+    <!-- Dialog -->
+    <!-- Resource Spec (YAML) -->
     <v-dialog v-model="editor.show" max-width="1000">
       <v-card>
         <v-card-title class="headline grey lighten-2" primary-title>
@@ -115,12 +127,13 @@
 
         <v-card-actions>
           <v-spacer></v-spacer>
-          <v-btn color="blue darken-1" flat @click="editor.show = false" v-if="false" disabled>Save</v-btn>
+          <v-btn color="blue darken-1" flat @click="editor.show = false" disabled>Save</v-btn>
           <v-btn color="blue darken-1" flat @click="editor.show = false">Close</v-btn>
         </v-card-actions>
       </v-card>
     </v-dialog>
 
+    <!-- SSH of Pod -->
     <v-dialog v-model="ssh.show" max-width="1000" lazy :fullscreen="ssh.full">
       <v-card>
         <v-card-title class="ssh-dialog headline grey lighten-2">
@@ -143,6 +156,7 @@
       </v-card>
     </v-dialog>
 
+    <!-- Logs of Pod -->
     <v-dialog v-model="log.show" max-width="1000" lazy>
       <v-card>
         <v-card-title class="headline grey lighten-2" primary-title>
@@ -165,30 +179,49 @@
 </template>
 
 <script>
+var meta = {
+  // Pod (Common)
+  "Name": "sortable | value=metadata.name | align=left | slot=link",
+  "Node": "value=kind",
+  "Status": "sortable | value=status.phase",
+  "Restarts": "value=value",
+  "Age":"sortable | value=metadata.creationTimestamp | slot=from-now",
+  "Action":"text= | value=none | slot=action",
+  // Deployment
+  "Deploy-Status":"text=Status | slot=replica",
+  "Deploy-Selector":"text=Selector | align=left | value=spec.selector.matchLabels | slot=label",
+  "Strategy":"value=spec.strategy.type",
+  "Images":"value=spec.selector.matchLabels | align=left | slot=images",
+  // Ingerss
+  "Endpoint":"slot=link | value=spec.rules[0].host", //TODO
+  // Service
+  "ClusterIP":"align=left | value=spec.clusterIP",
+  "Selector":"align=left | value=spec.selector | slot=label",
+}
+var COLS = {
+  deployment: 'Name,Deploy-Selector,Strategy,Images,Age,Action',
+  ing: 'Name,Endpoint,Age,Action',
+  svc: 'Name,Selector,ClusterIP,Endpoint,Age,Action',
+}
+var DEFAULT_COLS = 'Name,Node,Status,Restarts,Age,Action'
 
-function expandFields(meta){
+function expandFields(cols){
   var arr = [];
-  for(var name in meta){
+  for(var i in cols){
+    var name = cols[i];
     var m = meta[name].split(/ ?\| ?/);
-    var opt = {
-      text: name,
-      align: 'center',
-      sortable: false
-    }
+    var opt = {text: name, align: 'center', sortable: false, value: ''}
 
-    if(m.length){
-      for(var j in m){
-        var op1 = m[j].split("=")[0]
-        var op2 = m[j].split("=")[1]
-        switch(op1){
-          case 'sortable':
-            opt.sortable = true
-            break;
-          case 'value':
-          case 'align':
-            opt[op1] = op2
-            break;
-        }
+    for(var j in m){
+      var op1 = m[j].split("=")[0]
+      var op2 = m[j].split("=")[1]
+      switch(op1){
+        case 'sortable':
+          opt.sortable = true; break;
+        case 'value':
+        case 'align':
+        default:
+          opt[op1] = op2; break;
       }
     }
     arr.push(opt)
@@ -203,24 +236,16 @@ import 'codemirror/mode/yaml/yaml.js'
 const _ = require('underscore');
 
 export default {
-  name: 'Kube',
+  name: 'Resources',
   data () {
     return {
       pod:{},
       select: [
-        {
-          label: 'Cluster',
-          selected: 'zcp-demo',
-          items: []
-        },
-        {
-          label: 'Namespace',
-          selected: 'zcp-system',
-          items: []
-        },
+        {label: 'Cluster',   items: [], selected: 'zcp-demo'},
+        {label: 'Namespace', items: [], selected: 'zcp-system'},
         {
           label: 'Kind',
-          selected: undefined,
+          selected: '',
           items: [
             {text:"Node", value:"node"},
             {text:"Deployment", value:"deployment"},
@@ -237,12 +262,7 @@ export default {
           ]
         },
       ],
-      table: {
-        items: [],
-        loading: false,
-        keyword: "",
-        headers: []
-      },
+      table: {headers:[], items: [], loading: false, keyword: ""},
       editor: {
         show: false,
         selected: null,
@@ -256,64 +276,20 @@ export default {
           line: true
         }
       },
-      ssh: {
-        show: false,
-        cs: undefined,
-        ns: undefined,
-        pod: undefined,
-        full: false,
-        con: {}
-      },
-      log: {
-        show: false,
-        cs: undefined,
-        ns: undefined,
-        pod: undefined,
-        con: {}
-      }
+      ssh: {show: false, cs: undefined, ns: undefined, pod: undefined, con: {}, full: false },
+      log: {show: false, cs: undefined, ns: undefined, pod: undefined, con: {} }
     }
   },
   computed: {
-    headers (){
-      var meta = {
-        "Name": "sortable | value=metadata.name | align=left",
-        "Node": "value=kind",
-        "Status": "sortable | value=status.phase",
-        "Restarts": "value=value",
-        "Age":"sortable | value=status.startTime",
-        "":"value=none",
-      }
-
-      var kind = this.select[2].selected
-      if(kind == 'ing'){
-        meta.Endpoint = "value=spec.rules[0].host"
-      }
-
-      return expandFields(meta)
-    }
+    _: () => { return _ }
   },
   mounted() {
+    this.table.headers = this.getTabelHeaders()
+
     this.getCluster()
-    if(this.select[0].selected){
-      this.changeCluster()
-    }
+    this.select[0].selected && this.changeCluster()
   },
   methods: {
-    getPod () {
-      var api = '/api'
-
-      var cluster = this.select[0].selected;
-      var ns = this.select[1].selected;
-
-      this.table.loading = true
-      this.$http
-        .get(`${api}/cluster/${cluster}/namespace/${ns}/pod/list`)
-        .then((res)=>{
-          this.pod = res.data
-          this.table.items = res.data.items
-          this.table.loading = false
-        })
-    },
     getCluster() {
       var api = '/api'
 
@@ -356,16 +332,6 @@ export default {
       this.ssh.ns = this.select[1].selected;
       this.ssh.pod = item.metadata.name;
       this.ssh.con.items = _.map(item.spec.containers, (con) => {return con.name})
-
-      /*
-      var api = '/api'
-      this.$http
-        .get(`${api}/${kind}/${item.metadata.name}?type=yaml&cs=${cs}&ns=${ns}`)
-        .then((res)=>{
-          console.log(res)
-          this.editor.code = res.data
-        })
-      */
     },
     showLog (item) {
       this.log.show = true;
@@ -390,11 +356,19 @@ export default {
       this.$http
         .get(`${api}/${kind}/list?cs=${cs}&ns=${ns}`)
         .then((res)=>{
-          console.log(res)
-          //this.pod = res.data
+          this.table.headers = this.getTabelHeaders()
           this.table.items = res.data.items
           this.table.loading = false
         })
+    },
+    getTabelHeaders () {
+      var kind = this.select[2].selected;
+      var cols = COLS[kind] || DEFAULT_COLS
+      if(_.isString(cols)){
+        COLS[kind] = expandFields(cols.split(','))
+      }
+
+      return COLS[kind]
     }
   }
 }
