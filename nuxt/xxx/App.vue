979f17d9af1fba05375ddfc30a9ee670119f5550
Hoon Yang <hoon@172.20.nate.com>
2018-12-10 00:43:15 +0900

    nuxt style layout test

---
+++ nuxt/xxx/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <div id="app">
+    <vue-extend-layouts />
+  </div>
+</template>
+ 
+<script>
+import VueExtendLayouts from 'vue-extend-layout'
+ 
+export default {
+  name: 'App',
+  components: { VueExtendLayouts }
+}
+</script>
