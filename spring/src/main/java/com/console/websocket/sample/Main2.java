eb8c8df688cdca5f301053b3ab71d3d80e8a235f
Yang Hoon <ssang276@hanmail.net>
2018-12-09 16:16:25 +0900

    hooni working

--- spring/src/main/java/com/console/websocket/sample/Main2.java
+++ spring/src/main/java/com/console/websocket/sample/Main2.java
@@ -1,55 +1,100 @@
 package com.console.websocket.sample;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
-import java.util.concurrent.ScheduledExecutorService;
-import java.util.concurrent.ScheduledFuture;
 import java.util.concurrent.TimeUnit;
 
 import io.reactivex.Observable;
-import io.reactivex.ObservableEmitter;
-import io.reactivex.ObservableOnSubscribe;
-import io.reactivex.ObservableSource;
-import io.reactivex.functions.Function;
 
 // https://github.com/jacek-marchwicki/JavaWebsocketClient
 public class Main2 {
     public static void main(String[] args) {
         // https://github.com/ReactiveX/RxJava/wiki/Creating-Observables#create
-        Observable<WebSock> ob = Observable.create(new ClientHandshaker());
-        ob.flatMap(relayCreator)
-          .subscribe(System.out::println);
-
-        Future<WebSock> connectEvent = executor.schedule(()->{ return null; }, 10, TimeUnit.SECONDS);
+        handshaker()
+            .flatMap(Main2::relay)
+            .subscribe(System.out::println);
     }
 
     // https://github.com/ReactiveX/RxJava/wiki/Creating-Observables#fromfuture
-    private static ScheduledExecutorService executor = Executors.newSingleThreadScheduledExecutor();
-    private static class ClientHandshaker implements ObservableOnSubscribe {
-        public void subscribe(ObservableEmitter emitter) throws Exception {
-            emitter.onNext(new WebSock("1"));
-            emitter.onNext(new WebSock("2"));
-		}
+    // private static ScheduledExecutorService executor = Executors.newSingleThreadScheduledExecutor();
+    private static ExecutorService executor = Executors.newFixedThreadPool(2);
+    private static Observable<WebSock> handshaker(){
+        List<Future<WebSock>> list = new ArrayList<>();
+
+        int i = 2;
+        while(0 <= --i){
+            Future<WebSock> future = executor.submit(() -> {
+                Thread.currentThread().sleep(2000);
+                WebSock socket = new WebSock();
+                System.out.println("CONNECTED :: " + socket);
+                return socket;
+            });
+            list.add(future);
+        }
+
+        return Observable.fromIterable(list)
+                .flatMap(f -> Observable.fromFuture(f))
+                .delay(10, TimeUnit.SECONDS);
     }
 
     /*
-     * Subscriber
+     * Monad
      */
-    private static Function<WebSock, ObservableSource<Exchange>> relayCreator = new Function<WebSock, ObservableSource<Exchange>>() {
-        public ObservableSource<Exchange> apply(WebSock socket) throws Exception {
-            Exchange ex = new Exchange();
-            ex.in = socket;
-            ex.out = new WebSock("relay-of-" + socket.id);
-            return Observable.just(ex);
-        }
-    };
+    private static Observable<Exchange> relay(WebSock in){
+        Exchange ex = new Exchange();
+        ex.in = in;
+
+
+        // return Observable.just(ex);
+        Observable<String> info = runningServerInfo();
+        return info
+            .flatMap(server -> {
+                Future<WebSock> future = executor.submit(() -> {
+                    // Thread.currentThread().sleep(1000);
+                    WebSock socket = new WebSock("relay-of-" + in.id + "-to-" + server);
+                    // System.out.println("CONNECTED :: " + socket);
+                    return socket;
+                });
+                return Observable.fromFuture(future);
+            })
+            .flatMap(out -> {
+                ex.out = out;
+                return Observable.just(ex);
+            });
+    }
+    
+    private static Observable<String> running = null;
+    private static Observable<String> runningServerInfo(){
+        if(running != null)
+            return running;
+        
+        Future<String> future = executor.submit(() -> {
+            String server = "KUBE";
+
+            System.out.println("REQ :: " + server);
+            Thread.currentThread().sleep(1000 * 5);
+            System.out.println("RES :: " + server);
+            return server;
+        });
+
+		running = Observable.fromFuture(future);
+        return running;
+    }
 
     /*
      * Data Class
      */
     private static class WebSock {
+        private static int count = 0;
+
         public String id;
+
+        public WebSock() { this.id = String.valueOf(++count); }
         public WebSock(String id) { this.id = id; }
+
         public String toString() { return "WebSock(" + id + ")"; }
 
         public void send(String msg){
