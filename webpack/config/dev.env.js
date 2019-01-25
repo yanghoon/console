5ab6377e2aeeb0cb8f5234687f0d5176bcfc29e1
yanghoon <ssang276@hanmail.net>
2019-01-25 10:45:25 +0900

    support nuxt.env (in nuxt.config.js)

--- webpack/config/dev.env.js
+++ webpack/config/dev.env.js
@@ -4,4 +4,21 @@ const prodEnv = require('./prod.env')
 
 module.exports = merge(prodEnv, {
   NODE_ENV: '"development"'
+  // LAYOUT: '"default"'
 })
+
+/* START :: Customize */
+// https://hyunseob.github.io/2016/07/23/underscore-extend/
+const _ = require('underscore')
+
+_.extend(process.env, module.exports)
+const nuxt  = require('../nuxt.config.js')
+const nuxtEnvEscaped = _.mapObject(nuxt.env, (v) => {
+  if(_.isString(v))
+    return `"${v}"`
+  return v
+})
+console.log(nuxtEnvEscaped)
+
+_.extend(module.exports, nuxtEnvEscaped)
+/* END */
---
+++ webpack/nuxt.config.js
@@ -0,0 +1,7 @@
+// https://nuxtjs.org/guide/directory-structure#the-nuxt-config-js-file
+// https://ko.nuxtjs.org/api/configuration-env/
+module.exports = {
+  env: {
+    layout: process.env.LAYOUT || 'default'
+  }
+}
